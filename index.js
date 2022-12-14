8cfcb06f63875bca33b3d8cc0f96ff81de3f3cdd
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2022-12-14 09:33:16 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = 9000;
+
+app.use(cors({
+    origin: 'http://localhost:5500'
+}));
+
+app.use(express.json())
+
+app.post('/api', (req, res) => {
+    console.log(req.body);
+    res.status(200);
+    res.send(req.body);
+});
+
+app.listen(port, () => {
+    console.log(`Node Mirror listening on port ${port}`);
+});
