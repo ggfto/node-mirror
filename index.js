45f207c260de745364bc1f1a2085dab5a8736c8d
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2023-10-26 11:53:05 -0300

    bugfix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors({
     origin: 'http://localhost:5500'
 }));
 
-app.use(express.json())
+app.use(express.json());
 
 app.post('/api', (req, res) => {
     console.log(req.body);
@@ -20,10 +20,10 @@ app.get('/', (req, res) =>{
     res.send("Hello World");
 });
 
-app.post('lpr_readings/receive_readings', (req, res) => {
-    console.log(req.body);
+app.post('/lpr_readings/receive_readings', (req, res) => {
+    console.log(req.body)
     res.status(200);
-    res.send();
+    res.send({ code: 200, message: 'ok' });
 });
 
 app.listen(port, () => {
